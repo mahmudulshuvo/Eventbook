dc98fde504a84690c7a12e87741bad1b42c3f781
ShuvoHassan <shuvo0201@gmail.com>
2018-10-21 17:34:57 +0600

    Routing Configuration

--- src/features/nav/NavBar/Navbar.jsx
+++ src/features/nav/NavBar/Navbar.jsx
@@ -1,34 +1,57 @@
 import React, { Component } from "react";
 import { Menu, Container, Button } from "semantic-ui-react";
+import { NavLink, Link, withRouter } from "react-router-dom";
+import SignedOutMenu from "../Menus/SignedOutMenu";
+import SignedInMenu from "../Menus/SignedInMenu";
 
 class Navbar extends Component {
+  state = {
+    authenticated: false
+  };
+
+  handleSignIn = () => {
+    this.setState({
+      authenticated: true
+    });
+  };
+
+  handleSignOut = () => {
+    this.setState({
+      authenticated: false
+    });
+    this.props.history.push("/");
+  };
+
   render() {
     return (
       <div>
         <Menu inverted fixed="top">
           <Container>
-            <Menu.Item header>
-              <img src="assets/logo.png" alt="logo" />
+            <Menu.Item header as={Link} to="/">
+              <img src="/assets/logo.png" alt="logo" />
               Eventbook
             </Menu.Item>
-            <Menu.Item name="Events" />
-            <Menu.Item>
-              <Button
-                floated="right"
-                positive
-                inverted
-                content="Create Event"
-              />
-            </Menu.Item>
-            <Menu.Item position="right">
-              <Button basic inverted content="Login" />
-              <Button
-                basic
-                inverted
-                content="Sign Out"
-                style={{ marginLeft: "0.5em" }}
-              />
-            </Menu.Item>
+            <Menu.Item as={NavLink} to="/events" name="Events" />
+            {this.state.authenticated && (
+              <Menu.Item as={NavLink} to="/people" name="People" />
+            )}
+            {this.state.authenticated && (
+              <Menu.Item>
+                <Button
+                  as={Link}
+                  to="/createEvent"
+                  floated="right"
+                  positive
+                  inverted
+                  content="Create Event"
+                />
+              </Menu.Item>
+            )}
+            {this.state.authenticated ? (
+              <SignedInMenu signOut={this.handleSignOut} />
+            ) : (
+              <SignedOutMenu signIn={this.handleSignIn} />
+            )}
           </Container>
         </Menu>
       </div>
@@ -36,4 +59,4 @@ class Navbar extends Component {
   }
 }
 
-export default Navbar;
+export default withRouter(Navbar);
